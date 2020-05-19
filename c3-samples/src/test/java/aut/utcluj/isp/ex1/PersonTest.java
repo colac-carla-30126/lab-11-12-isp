3338cc650f0a701ac8b0a568f13523bb55ead276
colac-carla-30126 <colaccarla@gmail.com>
2020-05-19 04:39:33 +0300

    first

--- c3-samples/src/test/java/aut/utcluj/isp/ex1/PersonTest.java
+++ c3-samples/src/test/java/aut/utcluj/isp/ex1/PersonTest.java
@@ -2,7 +2,8 @@ package aut.utcluj.isp.ex1;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PersonTest {
     @Test
